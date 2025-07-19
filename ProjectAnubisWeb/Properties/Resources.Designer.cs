3250b4fb99549ad9744c7e08e3e4cdd6fb99b2b2
Цветелин Маринов <Cvetelinmarinov566@gmail.com>
2025-07-19 19:22:34 +0300

    New images

--- ProjectAnubisWeb/Properties/Resources.Designer.cs
+++ ProjectAnubisWeb/Properties/Resources.Designer.cs
@@ -113,6 +113,16 @@ namespace ProjectAnubisWeb.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap newTab {
+            get {
+                object obj = ResourceManager.GetObject("newTab", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap refresh {
             get {
                 object obj = ResourceManager.GetObject("refresh", resourceCulture);
